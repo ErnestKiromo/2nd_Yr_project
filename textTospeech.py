293e123242aae9b8a148597a82f1ddb5541f85ff
ErnestKiromo <ekiromo@gmail.com>
2024-04-03 13:14:51 +0300

    Apr 2nd 2024

--- textTospeech.py
+++ textTospeech.py
@@ -3,7 +3,7 @@ import pygame
 import os
 
 # Text to be converted to speech
-text = "Dreaming of a Future in Computer Science, ML, and AI is like..As a young student entering college or university, the world of Computer Science offers endless possibilities and opportunities for exploration. Many dream of delving into the realms of Machine Learning (ML) and Artificial Intelligence (AI), drawn by the excitement of innovation and the potential to shape the future.Pursuing Computer Science:For those passionate about technology and problem-solving, Computer Science (CS) provides a solid foundation. "
+text = "Dreaming of a Future in Computer Science, ML, and AI is like..As a young student entering college or university, the world of Computer Science offers endless possibilities and opportunities for exploration. Many dream of delving into the realms of Machine Learning (ML) and Artificial Intelligence (AI), drawn by the excitement of innovation and the potential to shape the future.Pursuing Computer Science:For those passionate about technology and problem-solving, Computer Science provides a solid foundation."
 
 # Create a gTTS object
 tts = gTTS(text=text, lang='en')
